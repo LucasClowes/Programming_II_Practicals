55e38ea8de79bca8d7fe1a807ab01ac5a108f582
jc428029 <lucas.clowes@my.jcu.edu.au>
2018-03-14 12:23:21 +1000

    initial commit

--- prac_03/oddNames.py
+++ prac_03/oddNames.py
@@ -2,12 +2,17 @@
 
 
 def main():
+    name = get_name()
+    for i in range(1, len(name), 2):
+        print(name[i])
+
+
+def get_name():
     name = input("Enter your name: ")
     while not name:
         print('Entry must not be blank')
         name = input("Enter your name: ")
-    for i in range(1, len(name), 2):
-        print(name[i])
+    return name
 
 
 main()
