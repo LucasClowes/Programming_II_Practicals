391bfaaf9df22c290cb4a403ad81483838b15745
jc428029 <lucas.clowes@my.jcu.edu.au>
2018-03-14 12:50:49 +1000

    add functions

--- prac_03/scores.py
+++ prac_03/scores.py
@@ -3,6 +3,7 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+
 # score = float(input("Enter score: "))
 # if score < 0:
 #     print("Invalid score")
@@ -15,13 +16,21 @@ Broken program to determine score status
 #         print("Excellent")
 # if score < 50:
 #     print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    grade = calculate_score(score)
+    print(grade)
 
-score = float(input("Enter score: "))
-if (score > 100) or (score < 0):
-    print("Invalid Score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("bad")
+
+def calculate_score(score):
+    if (score > 100) or (score < 0):
+        return ("Invalid Score")
+    elif score >= 90:
+        return ("Excellent")
+    elif score >= 50:
+        return ("Passable")
+    else:
+        return ("bad")
+
+
+main()
